85374f415b19dd1088ff83a726cdcec4c2ca54cb
Justin Bermudez <justinanbermudez@gmail.com>
2020-01-24 11:48:48 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,7 +12,8 @@ end
 
 def selection(num)
   if num = 1
-     p "YUM YUM MUNCH MUNCH MUNCH"
+     #p "YUM YUM MUNCH MUNCH MUNCH"
+     p "HAM HAM HAM IN MY TUMMY"
   elsif num = 2
      p "HAM HAM HAM IN MY TUMMY"
   end
